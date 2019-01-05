2c7b71ae8de9a656f17fdf26de9372718c37da39
Mikael Cederström <mikael.cederstrom@gmail.com>
2019-01-05 18:31:20 +0100

    Test

--- HelloWorld2/Program.cs
+++ HelloWorld2/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World2");
             Console.ReadKey();
             
         }
